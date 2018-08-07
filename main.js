cd928eede2e2ecbcc45714df73c75a4c881c77a4
tla111 <tlamarketing111@gmail.com>
2018-08-07 19:29:44 -0400

    made changes

--- main.js
+++ main.js
@@ -21,55 +21,58 @@
         
         // }
         //section 2 */
-        
-        
-let myName = prompt('What\'s your name?');
-console.log('What\'s your name?  ' + myName);
+       
+// Creating a function to initiate the program       
+ 
+function movieGame(){
+            
+    let myName = prompt('What\'s your name?');
+    console.log('What\'s your name?  ' + myName);
 
-let myMovie = confirm("Have you watched a movie lately?");
-console.log("Have you watched a movie lately? " + myMovie);
+    let myMovie = confirm("Have you watched a movie lately?");
+    console.log("Have you watched a movie lately? " + myMovie);
 
-if (myMovie === true) {
-    let myMovieLike = confirm("Did you like the movie?");
-    console.log("Did you like the movie? " + myMovieLike);
-    
-    let myMovieType = prompt("What type of movie was it?");
-    console.log("What type of movie was it? " + myMovieType);
-    let myWhatMovie = prompt("Which movie did you see?");
-    console.log("What type of movie was it? " + myWhatMovie);
-    let myMovieRecommend = confirm("Would you recommend this movie?");
-    console.log("Would you recommend this movie? " + myMovieRecommend);
-    if (myMovieRecommend === true){
-        alert("Great! I heard " + myWhatMovie + " is a great " + myMovieType + " movie.")
+    if (myMovie === true) {
+        let myMovieLike = confirm("Did you like the movie?");
+        console.log("Did you like the movie? " + myMovieLike);
+        
+        let myMovieType = prompt("What type of movie was it?");
+        console.log("What type of movie was it? " + myMovieType);
+        let myWhatMovie = prompt("Which movie did you see?");
+        console.log("What type of movie was it? " + myWhatMovie);
+        let myMovieRecommend = confirm("Would you recommend this movie?");
+        console.log("Would you recommend this movie? " + myMovieRecommend);
+        if (myMovieRecommend === true){
+            alert("Great! I heard " + myWhatMovie + " is a great " + myMovieType + " movie.")
+        } else {
+            alert("Awwww that sucks that you didn't like the movie " + myWhatMovie + " .")
+        }
+        
     } else {
-        alert("Awwww that sucks that you didn't like the movie " + myWhatMovie + " .")
+        alert("I guess " + myName + "you should check out some new movies.")
+        
     }
-    
-} else {
-    alert("I guess " + myName + "you should check out some new movies.")
-    
+
+
+
+
+    let elName = document.getElementById('name');
+    elName.innerHTML = myName
+
+    // let elDMV = document.getElementById('DMV');
+    // elDMV.innerHTML = myDMV
+
+    let elCity = document.getElementById('city')
+    elCity.innerHTML = myCity
+
+    let elYears = document.getElementById('years')
+    elYears.innerHTML = myYears
+
+
+
+    console.log(elName);
+    console.log(elCity);
+    console.log(elYears);
+
+
 }
-
-
-
-
-let elName = document.getElementById('name');
-elName.innerHTML = myName
-
-// let elDMV = document.getElementById('DMV');
-// elDMV.innerHTML = myDMV
-
-let elCity = document.getElementById('city')
-elCity.innerHTML = myCity
-
-let elYears = document.getElementById('years')
-elYears.innerHTML = myYears
-
-
-
-console.log(elName);
-console.log(elCity);
-console.log(elYears);
-
-
-
