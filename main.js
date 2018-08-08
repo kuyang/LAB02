58a28550e20d367791efa906a100b9aaf1dbad75
Greg Hsieh <greg.hsieh@gmail.com>
2018-08-08 19:39:52 -0400

    Editing Version

--- main.js
+++ main.js
@@ -1,31 +1,30 @@
-
-// let myDMV = confirm("Do you live in the DMV?");
-// console.log("Do you live in the DMV? " + myDMV);
-
-// let myCity = prompt("What city do you live in?");
-// console.log("What city do you live in? " + myCity);
-
-// let myYears = prompt("How many years have you lived there?")
-// console.log("How many years have you lived there? " + myYears);
-
-// let myMovieFav = prompt("What is your all time favorite movie?");
-// console.log("What is your all time favorite movie? " + myMovieFav);
-
-// alert("Would you recommend watching " + myMovieFav + " to your friends?")
-
-
-// if(myDMV === true) {
-    //     alert('Hello ' + myName + '!  DMV is the best region. Especially when you lived in ' + myCity + " " + myYears + " years.")
-    // } else {
-        //     alert("Hello " + myName + "! I've heard of " + myCity + ". How do like living in " +myCity + " after " +myYears+ " years?")
-        
-        // }
-        //section 2 */
-        
-        
 let myName = prompt('What\'s your name?');
 console.log('What\'s your name?  ' + myName);
 
+let myDMV = confirm("Do you live in the DMV?");
+console.log("Do you live in the DMV? " + myDMV);
+
+let myCity = prompt("What city do you live in?");
+console.log("What city do you live in? " + myCity);
+
+let myYears = prompt("How many years have you lived there?")
+console.log("How many years have you lived there? " + myYears);
+
+
+
+//section 2 */
+
+
+
+
+
+
+if(myDMV === true) {
+    alert('Hello ' + myName + '!  DMV is the best region. Especially when you have lived in ' + myCity + " for " + myYears + " years.")
+} else {
+    alert("Hello " + myName + "! I've heard of " + myCity + ". How do like living in " +myCity + " after " +myYears+ " years?")
+    
+}
 let myMovie = confirm("Have you watched a movie lately?");
 console.log("Have you watched a movie lately? " + myMovie);
 
@@ -43,33 +42,33 @@ if (myMovie === true) {
         alert("Great! I heard " + myWhatMovie + " is a great " + myMovieType + " movie.")
     } else {
         alert("Awwww that sucks that you didn't like the movie " + myWhatMovie + " .")
-    }
+    }    
     
 } else {
     alert("I guess " + myName + "you should check out some new movies.")
     
-}
+}    
 
 
 
 
-let elName = document.getElementById('name');
-elName.innerHTML = myName
+// let elName = document.getElementById('name');
+// elName.innerHTML = myName
 
 // let elDMV = document.getElementById('DMV');
 // elDMV.innerHTML = myDMV
 
-let elCity = document.getElementById('city')
-elCity.innerHTML = myCity
+// let elCity = document.getElementById('city')
+// elCity.innerHTML = myCity
 
-let elYears = document.getElementById('years')
-elYears.innerHTML = myYears
+// let elYears = document.getElementById('years')
+// elYears.innerHTML = myYears
 
 
 
-console.log(elName);
-console.log(elCity);
-console.log(elYears);
+// console.log(elName);
+// console.log(elCity);
+// console.log(elYears);
 
 
 
